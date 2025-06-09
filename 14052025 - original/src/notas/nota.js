d704b3aa05183fe7a1fedafc16b84503ecbf3fee
Thomaz <priamothomaz@gmail.com>
2025-06-09 18:33:31 -0300

    Add files via upload

--- 14052025 - original/src/notas/nota.js
+++ 14052025 - original/src/notas/nota.js
@@ -30,17 +30,21 @@ function mostrarDetalhes(alunoId, professorId, notaId, avaliacao, materiaId) {
 function limparDados() {
     mostrarDetalhes('', '', '', '', '');
 }
+//Função utilizada somente para limpar o filtro de matérias
 function limparFiltro(){
     dropDownFiltro.value = 0
 }
-
+//Função utilizada para salvar tanto uma alteração em um dado existente
+//Quanto para criar um novo registro
 async function funcaoSalvar() {
     const alunoId = dropdownAluno.value;
     const professorId = dropdownProfessor.value;
     const materiaId = dropdownMateria.value;
     const avaliacaoNota = dropdownAvaliacao.value;
 
-
+    //Vai verificar se campo ID foi preenchido dentro do modal
+    //Caso não tenha sido preenchido, quer dizer que não há um registro, então criará um registro
+    //Caso tenha sido preenchido, vai alterar os dados daquele registro selecionado
     if (modalIDnota.value === '') {
         await window.senacAPI.salvarNota(professorId, alunoId, materiaId, avaliacaoNota);
         limparDados()
@@ -52,7 +56,8 @@ async function funcaoSalvar() {
     await carregarnotas();
     limparDados();
 }
-
+//Função utilizada para excluir um registro do banco de dados
+//Quando foi executada, rodará um DELETE baseado no ID do registro e limpará os dados de dentro do modal
 async function excluirnota() {
     const pID = modalIDnota.value;
     if (pID) {
@@ -61,6 +66,8 @@ async function excluirnota() {
         limparDados();
     }
 }
+//Função utilizada para criar a funcionalidade do filtro de matérias
+//Roda um comando SQL onde utiliza um WHERE para filtrar os dados baseados na matéria selecionada
 async function filtrarNotas() {
     const filtro = dropDownFiltro.value; // obtém o valor já selecionado
     // Se a opção "Todas as Matérias" for selecionada (valor "0")
@@ -70,7 +77,8 @@ async function filtrarNotas() {
     }
     const filtroMaterias = await window.senacAPI.filtrarNota(filtro);
     tabelanota.innerHTML = '';
-    
+    //Verifica caso não há dados para preencher os registros
+    //Se não houver, exibirá a mensagem "Sem dados", caso tenha, irá exibir normalmente
     if (!filtroMaterias || filtroMaterias.length === 0) {
         const linha = document.createElement("tr");
         const celula = document.createElement("td");
@@ -80,15 +88,18 @@ async function filtrarNotas() {
     } else {
         filtroMaterias.forEach(criarLinhanota);
     }
+    //Iniciador para os codigos de icones na função criarLinhanota
     lucide.createIcons();
 }
+//Função utilizada para carregar todos os dados da tabela notas sem a intervenção do filtro de matérias
+//Carregará todos os dados da tabela de notas utilizando a função declara na API senacAPI
 async function carregarnotas() {
     if (dropDownFiltro.value=0){
         filtrarNotas(filtro)
     }
     const listanotas = await window.senacAPI.buscarNota();
     tabelanota.innerHTML = "";
-
+    //Caso não houver dados, carregara a mensagem "Sem dados"
     if (!listanotas || listanotas.length === 0) {
         const linha = document.createElement("tr");
         const celula = document.createElement("td");
@@ -99,13 +110,16 @@ async function carregarnotas() {
     }
 
     listanotas.forEach(criarLinhanota);
+    //Iniciador para os codigos de icones da função criarLinhanota
     lucide.createIcons();
 }
+//Função utilizada para carregar cada linha dos registros da tabela de notas
 
 function criarLinhanota(nota) {
-    const linha = document.createElement("tr");
-    const celulaIconeAvaliacao = document.createElement("td")
-    if (nota.avaliacao ==='Aprovado'){
+
+    const linha = document.createElement("tr"); //Cria o corpo da tabela
+    const celulaIconeAvaliacao = document.createElement("td")//Cria a coluna que carregará os icones de aprovado ou reprovado
+    if (nota.avaliacao ==='Aprovado'){ //Verifica se o valor da coluna 'avaliacao' é Aprovado ou Reprovado e cria um insere um icone baseado nisto
     celulaIconeAvaliacao.innerHTML='<i data-lucide = "smile" class= "aprovado"></i>'
     linha.appendChild(celulaIconeAvaliacao)
 }
@@ -113,31 +127,34 @@ function criarLinhanota(nota) {
     celulaIconeAvaliacao.innerHTML='<i data-lucide ="frown" class="reprovado"></i>'
     linha.appendChild(celulaIconeAvaliacao)    
     }
-    
+    //Cria a coluna que armazenará o nome do aluno vinculado a nota
+    const celulaAluno = document.createElement("td");//Cria a coluna
+    celulaAluno.textContent = nota.aluno;//Define o conteúdo dela
+    linha.appendChild(celulaAluno);//Insere a Coluna no corpo da tabela
 
-    const celulaAluno = document.createElement("td");
-    celulaAluno.textContent = nota.aluno;
-    linha.appendChild(celulaAluno);
-
-    const celulaMateria = document.createElement("td");
+    //Cria a coluna que armazenará o nome da matéria vinculada a nota
+    const celulaMateria = document.createElement("td"); 
     celulaMateria.textContent = nota.materia;
     linha.appendChild(celulaMateria);
 
+    //Cria a coluna que armazenará o nome do professor vinculado a nota
     const celulaProfessor = document.createElement("td");
     celulaProfessor.textContent = nota.professor;
     linha.appendChild(celulaProfessor);
 
+    //Cria a coluna que armazenará a avaliação do aluno
     const celulaAvaliacao = document.createElement("td");
     celulaAvaliacao.textContent = nota.avaliacao;
     linha.appendChild(celulaAvaliacao);
-
-    const celulaBotao = document.createElement("td");
-    const botao = document.createElement("button");
-    botao.innerHTML = `<i data-lucide="edit"></i>`;
+    //Cria o botão da lateral direita
+    const celulaBotao = document.createElement("td");//Cria a coluna do botão
+    const botao = document.createElement("button");//Cria o botão que será vinculado a coluna
+    botao.innerHTML = `<i data-lucide="edit"></i>`;//Define o ícone do botão
     botao.addEventListener("click", () => {
         mostrarDetalhes(nota.aluno_id, nota.professor_id, nota.id, nota.avaliacao, nota.materia_id);
-    });
+    }); //Cria o listener que irá executar a função de mostrarDetalhes, onde irá inserir os dados da nota no modal caso seja clicado
     
+    //Cria todos os icones dos botões que já estão criados no HTML
    const iconeIncluir = document.getElementById('iIncluir');
 if (iconeIncluir) iconeIncluir.setAttribute('data-lucide', 'plus');
 
@@ -157,7 +174,7 @@ if (iconeLimparFiltro) iconeLimparFiltro.setAttribute('data-lucide', 'refresh-cc
 
     tabelanota.appendChild(linha);
 }
-
+//Cria os DropDowns dos dados vinculados a nota, para que sejam alterados ou salvos 
 async function preencherDropdownAlunos() {
     const alunos = await window.senacAPI.buscarAlunos();
     dropdownAluno.innerHTML = "";
