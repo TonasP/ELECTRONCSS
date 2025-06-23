10b98ed37d6aecd6d131e804946c44a00fac0f55
Thomaz <priamothomaz@gmail.com>
2025-06-23 19:29:05 -0300

    Update appListeners.js

--- ProjetoIntegrador/src/appListeners.js
+++ ProjetoIntegrador/src/appListeners.js
@@ -1,20 +1,77 @@
-const { ipcMain } = require('electron')
-const {
-    buscarAgendamentos,
-    deletarAgendamento,
-    alterarAgendamento,
-    salvarAgendamento,
-} = require('./agendamento/agendamentoDB');
+const { ipcMain } = require('electron');
+
+// Importe os módulos de banco de dados
+const agendamentoDB = require('./agendamento/agendamentoDB');
+const clienteDB = require('./cliente/clienteDB');
+const funcionarioDB = require('./funcionario/funcionarioDB');
+const pagamentosDB = require('./pagamentos/pagamentosDB');
+const planoDB = require('./plano/planoDB');
+const servicoDB = require('./servico/servicoDB');
+const janelas = require('./janelasModal')
+
+// --- Handlers por Entidade ---
 
 function registrarAgendamentoHandler() {
-    ipcMain.handle('buscar-Agendamentos', buscarAgendamentos);
-    ipcMain.handle('deletar-Agendamentos', deletarAgendamento);
-    ipcMain.handle('alterar-Agendamento', alterarAgendamento)
-    ipcMain.handle('salvar-Agendamento', salvarAgendamento)
+    ipcMain.handle('buscar-agendamentos', agendamentoDB.buscarAgendamentos);
+    ipcMain.handle('deletar-agendamento', agendamentoDB.deletarAgendamento);
+    ipcMain.handle('alterar-agendamento', agendamentoDB.alterarAgendamento);
+    ipcMain.handle('salvar-agendamento', agendamentoDB.salvarAgendamento);
+    ipcMain.handle('filtrar-agendamentos',agendamentoDB.filtrarAgendamentos);
 }
+
+function registrarClienteHandler() {
+    ipcMain.handle('buscar-clientes', clienteDB.buscarClientes);
+    ipcMain.handle('deletar-cliente', clienteDB.deletarCliente);
+    ipcMain.handle('alterar-cliente', clienteDB.alterarCliente);
+    ipcMain.handle('salvar-cliente', clienteDB.salvarCliente);
+}
+
+function registrarFuncionarioHandler() {
+    ipcMain.handle('buscar-funcionarios', funcionarioDB.buscarFuncionarios);
+    ipcMain.handle('deletar-funcionario', funcionarioDB.deletarFuncionario);
+    ipcMain.handle('alterar-funcionario', funcionarioDB.alterarFuncionario);
+    ipcMain.handle('salvar-funcionario', funcionarioDB.salvarFuncionario);
+}
+
+function registrarPagamentoHandler() {
+    ipcMain.handle('buscar-pagamentos', pagamentosDB.buscarPagamentos);
+    ipcMain.handle('deletar-pagamento', pagamentosDB.deletarPagamento);
+    ipcMain.handle('alterar-pagamento', pagamentosDB.alterarPagamento);
+    ipcMain.handle('salvar-pagamento', pagamentosDB.salvarPagamento);
+}
+
+function registrarPlanoHandler() {
+    ipcMain.handle('buscar-planos', planoDB.buscarPlanos);
+    ipcMain.handle('deletar-plano', planoDB.deletarPlano);
+    ipcMain.handle('alterar-plano', planoDB.alterarPlano);
+    ipcMain.handle('salvar-plano', planoDB.salvarPlano);
+}
+
+function registrarServicoHandler() {
+    ipcMain.handle('buscar-servicos', servicoDB.buscarServicos);
+    ipcMain.handle('deletar-servico', servicoDB.deletarServico);
+    ipcMain.handle('alterar-servico', servicoDB.alterarServico);
+    ipcMain.handle('salvar-servico', servicoDB.salvarServico);
+}
+
+function registrarJanelas() {
+    ipcMain.on('abrir-agendamento', janelas.modalAbrirAgendamentos)
+    ipcMain.on('abrir-cliente', janelas.modalAbrirClientes)
+    
+}
+
+// --- Função Principal para Registrar Todos os Listeners ---
+
 function registrarListeners() {
-    registrarAgendamentoHandler
+    registrarAgendamentoHandler();
+    registrarClienteHandler();
+    registrarFuncionarioHandler();
+    registrarPagamentoHandler();
+    registrarPlanoHandler();
+    registrarServicoHandler();
+    registrarJanelas()
 }
+
 module.exports = {
     registrarListeners
-}
+};
